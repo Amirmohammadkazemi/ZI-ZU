832bea9249f22ded0175e23b524bf2c0b653ea1e
Markson Hon <50002150+MarksonHon@users.noreply.github.com>
2024-07-21 02:22:40 +0800

    start again

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,7 @@
 extern crate flate2;
-
-use std::io::BufReader;
-use flate2::write::GzEncoder;
-use flate2::Compression;
 use std::env::args;
-use std::fs::{File};
-use std::io::copy;
-use std::time::Instant;
 use std::process::exit;
+
 
 fn show_help() {
     println!("HELLLLLLLLLPPPPPPPPP")
@@ -17,25 +11,8 @@ fn show_info() {
     println!("ZI-ZU v1.0.0");
 }
 
-pub fn zipping(inp_file: String, target: String) {
-    println!("");
-    println!("------------------------");
-    println!("inpup file: {:?}", inp_file);
-    println!("output file: {:?}", target);
-
-        let mut input: BufReader<File> = BufReader::new(File::open(inp_file).unwrap());
-        let output: File = File::create(target).unwrap();
-        let mut encoder: GzEncoder<File> = GzEncoder::new(output, Compression::default());
-        let start:Instant = Instant::now();
-        copy(&mut input, &mut encoder).unwrap();
-        let output: File = encoder.finish().unwrap();
-        println!("------------------------");
-        println!(
-            "Source len: {:?}",
-            input.get_ref().metadata().unwrap().len()
-        );
-        println!("Target len: {:?}", output.metadata().unwrap().len());
-        println!("Elapsed: {:?}", start.elapsed());
+fn zipping(){
+    println!("Zipping");
 }
 
 pub fn unzip() {
@@ -52,7 +29,7 @@ fn main() {
         };
         match arg.as_str() {
             "-z" | "--zip" => {
-                zipping(args.next().unwrap(), args.next().unwrap());
+                zipping();
             }
             "-u" | "--unzip" => {
                 println!("Unzipping");
